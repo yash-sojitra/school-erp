16bde056727b09f0b8011582c7dfdbc74ce0cd00
yashsojitra97 <yashsojitra97@gmail.com>
2024-05-20 16:17:38 +0530

    sidebar commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,59 @@
 // import { useState } from 'react'
 import './App.css'
-import StudentDashboard from './modules/student/StudentDashboard'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Dashboard from './modules/student/Dashboard';
+import HomePage from './modules/student/pages/HomePage';
+import Library from './modules/student/pages/Library';
+import Academics from './modules/student/pages/Academics';
+import EventsPage from './modules/student/pages/EventsPage';
+import Support from './modules/student/pages/Support';
+import ChatPage from './modules/student/pages/ChatPage';
+import FeesPayment from './modules/student/pages/FeesPayment';
+
+
+const router = createBrowserRouter([
+  {
+    path:"/student",
+    element: <Dashboard />,
+    children: [
+      {
+        path: "/student/home",
+        element: <HomePage />,
+      },
+      {
+        path: "/student/library",
+        element: <Library />,
+      },
+      {
+        path: "/student/academics",
+        element: <Academics />,
+      },
+      {
+        path: "/student/events",
+        element: <EventsPage />,
+      },
+      {
+        path: "/student/support",
+        element: <Support />,
+      },
+      {
+        path: "/student/chat",
+        element: <ChatPage />,
+      },
+      {
+        path: "/student/fees",
+        element: <FeesPayment />,
+      },
+    ],
+  },
+]);
 
 function App() {
 
   return (
-    <>
-      <StudentDashboard/>
-    </>
+    <div className="flex">
+      <RouterProvider router={router} />
+    </div>
   )
 }
 
